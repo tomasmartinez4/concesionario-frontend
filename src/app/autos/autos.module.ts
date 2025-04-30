f360abdd40437e8ea532a13b8db333a35212eca8
Tomas Martinez <tomas44martinez@gmail.com>
2025-04-30 13:07:29 -0300

    new updates

--- src/app/autos/autos.module.ts
+++ src/app/autos/autos.module.ts
@@ -1,10 +1,8 @@
-// src/app/autos/autos.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListadoAutosComponent } from './pages/listado-autos/listado-autos.component';
 
 const routes: Routes = [
-  // Cuando entres a /autos, muestra ListadoAutosComponent
   { path: '', component: ListadoAutosComponent }
 ];
 
